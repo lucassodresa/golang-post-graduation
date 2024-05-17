3bb3d8127bc266f513ba5724d4d6de81f7354caa
lucassodresa <lucas.sodresa@gmail.com>
2024-05-17 18:31:54 +0100

    study progress

---
+++ chapter-01-foundation/09-variadic-functions/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println(sum(1, 2, 3, 4, 5))
+
+}
+
+func sum(numbers ...int) int {
+	total := 0
+
+	for _, number := range numbers {
+		total += number
+	}
+
+	return total
+}

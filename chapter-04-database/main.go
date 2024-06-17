8b74eb3b860c002917c10958047cabd7a6391506
lucassodresa <lucas.sodresa@gmail.com>
2024-06-17 22:24:46 +0100

    study progress

--- chapter-04-database/main.go
+++ chapter-04-database/main.go
@@ -1,9 +1,14 @@
 package main
 
-import "github.com/google/uuid"
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
 
 type Product struct {
-	ID    int
+	ID    string
 	Name  string
 	Price float64
 }
@@ -17,4 +22,27 @@ func NewProduct(name string, price float64) *Product {
 }
 
 func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	product := NewProduct("TV", 199.99)
+	err = insertProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func insertProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("INSERT INTO products (id, name, price) VALUES (?, ?, ?)")
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		panic(err)
+	}
+	return nil
 }
